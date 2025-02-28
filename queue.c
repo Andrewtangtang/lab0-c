063c6ababdaea3db5a0e2b51ea70708bb9d51e25
Yun-Tang Chang <yuntang1113@gmail.com>
2025-02-28 14:38:17 +0000

    Implement q_reverse function
    Implements a circular linked list reversal function to change nodes
    order. The implementation uses in-place pointer swapping with a
    single do-while loop traversal.
    
    Change-Id: Iff30817d0a7a9bbb8c16078820632301f62ca67b

--- queue.c
+++ queue.c
@@ -178,8 +178,19 @@ void q_swap(struct list_head *head)
     // https://leetcode.com/problems/swap-nodes-in-pairs/
 }
 
-/* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+
+    struct list_head *curr = head;
+    do {
+        struct list_head *tmp = curr->next;
+        curr->next = curr->prev;
+        curr->prev = tmp;
+        curr = tmp;
+    } while (curr != head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
